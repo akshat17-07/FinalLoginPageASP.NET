8750b4b462df70962effcd16733abcc436c690d3
Akshat Harlalka <avh5936@psu.edu>
2023-05-30 06:11:47 -0400

    Able to Create users

---
+++ LoginRegister/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using LoginRegister.Data;
+using LoginRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegister.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserDbContext _database;
+
+        public LoginController(UserDbContext database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserModel model)
+        {
+            _database.LoginDetail.Add(model);
+            _database.SaveChanges();
+
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
